364d47699f7224e4085cfbec45940d15f719a120
manzomonza <massimiliano.manzo@posteo.ch>
2023-08-17 13:42:56 +0200

    Revert to initial function call and commented troubleshoot code

--- call_VCFparse.R
+++ call_VCFparse.R
@@ -32,47 +32,47 @@ print("Packages: loaded")
 
 # COMMENT SECTION
 
-#write_parsed_output(vcf = vcf, vcf_path = vcfpath, parsed_fp = parsed_fp  )
-
-print("TROUBLESHOOT")
-
-metainf = parse_vcfpath_return_metainformation(vcfpath)
-print(paste0("metainf of vcf:", metainf))
-
-analysis_name = metainf$IonReporter$AnalysisName
-print(paste0("base name of analysis_name:", analysis_name))
-
-vcf_file = basename(vcfpath)
-print(paste0("base name of vcfpath:", vcf_file))
-
-print("complete file")
-if(nrow(vcf) > 0){
-  vcf_w = attach_ID(vcf, vcf_file = vcf_file, analysis_name = analysis_name)
-  readr::write_tsv(vcf_w, file = parsed_fp$parsed_complete)
-}
-print("snv file")
-
-snv = parse_vcf_return_snv(vcf)
-if(nrow(snv) > 0){
-  snv = attach_ID(snv, vcf_file = vcf_file, analysis_name = analysis_name)
-  readr::write_tsv(snv, file = parsed_fp$parsed_snv)
-}
-print("cnv file")
-
-cnv = parse_vcf_return_cnv(vcf)
-if(nrow(cnv) > 0){
-  cnv = attach_ID(cnv, vcf_file = vcf_file, analysis_name = analysis_name)
-  readr::write_tsv(cnv, file = parsed_fp$parsed_cnv)
-}
-
-## Meta information
-print("meta file")
-write_out_META_information(metainf, filename = parsed_fp$parsed_info)
-
-print("filepaths file")
-## Parsed filepath info
-write_parsed_fp_txt(parsed_fp, vcf_file = 'no1cares', analysis_name = 'no1cares')
-
+write_parsed_output(vcf = vcf, vcf_path = vcfpath, parsed_fp = parsed_fp)
+#
+# print("TROUBLESHOOT")
+#
+# metainf = parse_vcfpath_return_metainformation(vcfpath)
+# print(paste0("metainf of vcf:", metainf))
+#
+# analysis_name = metainf$IonReporter$AnalysisName
+# print(paste0("base name of analysis_name:", analysis_name))
+#
+# vcf_file = basename(vcfpath)
+# print(paste0("base name of vcfpath:", vcf_file))
+#
+# print("complete file")
+# if(nrow(vcf) > 0){
+#   vcf_w = attach_ID(vcf, vcf_file = vcf_file, analysis_name = analysis_name)
+#   readr::write_tsv(vcf_w, file = parsed_fp$parsed_complete)
+# }
+# print("snv file")
+#
+# snv = parse_vcf_return_snv(vcf)
+# if(nrow(snv) > 0){
+#   snv = attach_ID(snv, vcf_file = vcf_file, analysis_name = analysis_name)
+#   readr::write_tsv(snv, file = parsed_fp$parsed_snv)
+# }
+# print("cnv file")
+#
+# cnv = parse_vcf_return_cnv(vcf)
+# if(nrow(cnv) > 0){
+#   cnv = attach_ID(cnv, vcf_file = vcf_file, analysis_name = analysis_name)
+#   readr::write_tsv(cnv, file = parsed_fp$parsed_cnv)
+# }
+#
+# ## Meta information
+# print("meta file")
+# write_out_META_information(metainf, filename = parsed_fp$parsed_info)
+#
+# print("filepaths file")
+# ## Parsed filepath info
+# write_parsed_fp_txt(parsed_fp, vcf_file = vcf_file, analysis_name = vcf_file)
+#
 
 
 
