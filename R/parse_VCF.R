2775076cbb661662571262c0deabfb1013c13bbc
manzomonza <massimiliano.manzo@posteo.ch>
2023-11-21 13:03:25 +0100

    Removed filepath functions to rethink file listing strategy

--- R/parse_VCF.R
+++ R/parse_VCF.R
@@ -29,7 +29,12 @@ parse_vcf_return_snv = function(vcf){
 parse_vcf_return_cnv = function(vcf){
   cnv_rows = dplyr::filter(vcf, alt == "<CNV>")
   if(nrow(cnv_rows) > 0){
-    cnv_rows = cnv_parse(cnv_rows)
+    cnv_rows = dplyr::select(cnv_rows, seqnames, gene, NUMTILES, CI, CN, PVAL)
+    cnv_rows = dplyr::distinct(cnv_rows)
+    cnv_rows = tidyr::unnest(cnv_rows, CI)
+    cnv_rows$CI = paste0('perc_',cnv_rows$CI)
+    cnv_rows = tidyr::separate(cnv_rows, CI, into = c("percentile", "value"), sep = ":")
+    cnv_rows = tidyr::pivot_wider(cnv_rows, names_from = "percentile", values_from = 'value')
     cnv_rows = dplyr::arrange(cnv_rows, desc(CN))
   }
     return(cnv_rows)
--- call_VCFparse.R
+++ call_VCFparse.R
@@ -1,7 +1,4 @@
 ## VCFparse pipeline
-library(VCFparse)
-library(VariantAnnotation)
-library(yaml)
 
 
 ################################################# OPTPARSE
@@ -17,61 +14,40 @@ library(yaml)
 # print(opt$file)
 
 #################################################
+##
+path_parsed_output = paste0(analysis_dir, "/parsed_output") # analysis_dir gets inherited from genie.R
+if(!dir.exists(path_parsed_output)){
+  dir.create(path_parsed_output)
+}
+
+# parsed_fusions = paste0(path_parsed_output, '/parsed_fusions.tsv') # not implemented yet
 
 
-#   dplyr::mutate_all(as.character) |>
-# tibble::as_tibble(vcf) |>
-#   tidyr::pivot_longer(-gene)|>
-#   dplyr::filter(grepl("\\[", value)) |>
-#   dplyr::filter(name != 'FUNC')
 
 
+metainf = parse_vcfpath_return_metainformation(vcfpath)
+analysis_name = metainf$IonReporter$AnalysisName
+vcf_file = basename(vcfpath)
 
+## Output files
+if(nrow(vcf) > 0){
+  vcf_w = attach_ID(vcf, vcf_file = vcf_file, analysis_name = analysis_name)
+  vcf_w = remove_multientry_leftover_characters(vcf_w)
+  readr::write_tsv(vcf_w, file = paste0(path_parsed_output, '/parsed_complete.tsv'))
+}
+snv = parse_vcf_return_snv(vcf)
+snv = remove_multientry_leftover_characters(snv)
+if(nrow(snv) > 0){
+  snv = attach_ID(snv, vcf_file = vcf_file, analysis_name = analysis_name)
+  readr::write_tsv(snv, file = paste0(path_parsed_output, '/parsed_snv.tsv'))
+}
+cnv = parse_vcf_return_cnv(vcf)
+if(nrow(cnv) > 0){
+  cnv = attach_ID(cnv, vcf_file = vcf_file, analysis_name = analysis_name)
+  readr::write_tsv(cnv, file = paste0(path_parsed_output, '/parsed_cnv.tsv'))
+}
 
-# COMMENT SECTION
-
-write_parsed_output(vcf = vcf, vcf_path = vcfpath, parsed_fp = parsed_fp)
-#
-# print("TROUBLESHOOT")
-#
-# metainf = parse_vcfpath_return_metainformation(vcfpath)
-# print(paste0("metainf of vcf:", metainf))
-#
-# analysis_name = metainf$IonReporter$AnalysisName
-# print(paste0("base name of analysis_name:", analysis_name))
-#
-# vcf_file = basename(vcfpath)
-# print(paste0("base name of vcfpath:", vcf_file))
-#
-# print("complete file")
-# if(nrow(vcf) > 0){
-#   vcf_w = attach_ID(vcf, vcf_file = vcf_file, analysis_name = analysis_name)
-#   readr::write_tsv(vcf_w, file = parsed_fp$parsed_complete)
-# }
-# print("snv file")
-#
-# snv = parse_vcf_return_snv(vcf)
-# if(nrow(snv) > 0){
-#   snv = attach_ID(snv, vcf_file = vcf_file, analysis_name = analysis_name)
-#   readr::write_tsv(snv, file = parsed_fp$parsed_snv)
-# }
-# print("cnv file")
-#
-# cnv = parse_vcf_return_cnv(vcf)
-# if(nrow(cnv) > 0){
-#   cnv = attach_ID(cnv, vcf_file = vcf_file, analysis_name = analysis_name)
-#   readr::write_tsv(cnv, file = parsed_fp$parsed_cnv)
-# }
-#
-# ## Meta information
-# print("meta file")
-# write_out_META_information(metainf, filename = parsed_fp$parsed_info)
-#
-# print("filepaths file")
-# ## Parsed filepath info
-# write_parsed_fp_txt(parsed_fp, vcf_file = vcf_file, analysis_name = vcf_file)
-#
-
-
+## Meta information
+write_out_META_information(metainf, filename = paste0(path_parsed_output, '/parsed_vcf_info.yaml'))
 
 
